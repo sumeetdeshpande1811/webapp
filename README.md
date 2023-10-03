ddf4df7cdb5de0df402eaabcadc04f0bef690652
sumeetdeshpande1811 <deshpande.su@northeastern.edu>
2023-10-03 14:57:57 -0400

    Revert "Adding webapp"
    This reverts commit 0b371cf6c23d6e11ddbe98c80e1f369e8af7df0b.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-Assignment 3
-Final repo
+# webapp
--- routes/healthCheckRoute.js
+++
@@ -1,6 +0,0 @@
-const express=require('express');
-const router =express.Router();
-const healthCheckController=require('../controller/healthCheckController');
-router.get('/healthz', healthCheckController);
-
-module.exports = router;
