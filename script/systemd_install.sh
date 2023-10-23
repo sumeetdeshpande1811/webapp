0041d2e14e1e211ff58125082c592b51e1dc5a71
sumeetdeshpande1811 <deshpande.su@northeastern.edu>
2023-10-23 17:28:54 -0400

    adding systemd

---
+++ script/systemd_install.sh
@@ -0,0 +1,6 @@
+sudo cp /tmp/webapp.service /lib/systemd/system/webapp.service
+echo "Service File successfully copied"
+sudo systemctl daemon-reload
+sudo systemctl start webapp
+sudo systemctl status webapp
+sudo systemctl enable webapp
