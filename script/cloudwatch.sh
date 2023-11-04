b776a3a8620d62934b22f447798de9107bbe905c
sumeetdeshpande1811 <deshpande.su@northeastern.edu>
2023-11-04 15:36:43 -0400

    adding logger code

--- script/cloudwatch.sh
+++ script/cloudwatch.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
-
 sudo wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i -E amazon-cloudwatch-agent.deb
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
-    -a fetch-config \
-    -m ec2 \
-    -c file:/opt/csye6225/aws_cw_config.json \
-    -s
 sudo systemctl enable amazon-cloudwatch-agent
 sudo systemctl start amazon-cloudwatch-agent
