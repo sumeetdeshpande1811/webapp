5921227d542a04297cd4515892676b461133cb66
sumeetdeshpande1811 <deshpande.su@northeastern.edu>
2023-11-24 10:05:41 -0500

    adding aws cli script

--- script/aws-cli-setup.sh
+++ script/aws-cli-setup.sh
@@ -3,4 +3,4 @@ sudo apt-get install -y awscli
 # AWS CLI configuration
 aws configure set aws_access_key_id "YOUR_ACCESS_KEY"
 aws configure set aws_secret_access_key "YOUR_SECRET_KEY"
-aws configure set region "YOUR_REGION"
+aws configure set region "us-east-1"
