649d9d0de3ec685b741f11e31cec9649bcf7454e
sumeetdeshpande1811 <deshpande.su@northeastern.edu>
2023-11-24 12:11:26 -0500

    change permission

--- script/aws-cli-setup.sh
+++ script/aws-cli-setup.sh
@@ -5,5 +5,7 @@ aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID" --profile dev
 aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY" --profile dev
 aws configure set region "us-east-1" --profile dev
 
-sudo cp .aws /opt/csye6225/
-chown -R csye6225:csye6225 /opt/csye6225/.aws/credentials
+sudo cp -r .aws /opt/csye6225/
+
+
+sudo chown -R csye6225:csye6225 /opt/csye6225/.aws
