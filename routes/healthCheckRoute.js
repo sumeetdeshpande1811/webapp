00562e5faa691fafd77da3157bf26418c2ad5598
sumeetdeshpande1811 <deshpande.su@northeastern.edu>
2023-12-02 14:30:37 -0500

    update code for CD

--- routes/healthCheckRoute.js
+++ routes/healthCheckRoute.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const router =express.Router();
 const healthCheckController=require('../controller/healthCheckController');
-router.use('/health', healthCheckController);
+router.use('/healthz', healthCheckController);
 
 module.exports = router;
